25a7bc8ffae110c068ec069ede752ab4699a14a5
Josh S <aaabccc87@protonmail.com>
2022-12-04 01:41:11 -0500

    Firebase config info

---
+++ src/config/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-MjMGv5SodSY0AO82pHWCj0mGuMRGPZ4",
+  authDomain: "odin-where-s-waldo.firebaseapp.com",
+  projectId: "odin-where-s-waldo",
+  storageBucket: "odin-where-s-waldo.appspot.com",
+  messagingSenderId: "236653238406",
+  appId: "1:236653238406:web:3c53a53c2a35a80c9c9907"
+};
+
+export default firebaseConfig;
