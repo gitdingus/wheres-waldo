eab1020657ab316751d2c654c75945c39ec1ddca
Josh S <aaabccc87@protonmail.com>
2022-12-08 17:28:09 -0500

    Build element that displays gameboards stored in firebase

---
+++ src/Game.js
@@ -0,0 +1,41 @@
+import {
+    getFirestore,
+    doc,
+    getDocs,
+    collection
+} from 'firebase/firestore';
+
+class Game {
+    static async getGameboardsElement(app) {
+        const db = getFirestore(app);
+        const gameboardsCollectionRef = collection(db, 'gameboards');
+        const snapshot = await getDocs(gameboardsCollectionRef);
+        const gameboardsDiv = document.createElement('div');
+
+        gameboardsDiv.classList.add('gameboards');
+
+        snapshot.forEach((gameboard) => {
+            const gameboardData = gameboard.data();
+            const gameboardDiv = document.createElement('div');
+            const gameboardTitle = document.createElement('h2');
+            const gameboardImg = document.createElement('img');
+            
+            gameboardDiv.classList.add('gameboard-preview');
+            gameboardDiv.setAttribute('data-id', gameboard.id);
+
+            gameboardTitle.textContent = gameboardData.title;
+            gameboardImg.src = gameboardData.image;
+
+            gameboardDiv.appendChild(gameboardImg);
+            gameboardDiv.appendChild(gameboardTitle);
+
+            gameboardsDiv.appendChild(gameboardDiv);
+        });
+
+        return gameboardsDiv;
+    }
+
+
+}
+
+export default Game;
